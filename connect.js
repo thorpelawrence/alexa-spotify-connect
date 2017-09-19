9581fe1ab4782fd5fb0d1d495d1db50346f3b2b1
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2017-09-19 20:42:07 +0100

    Cleanup comments

--- connect.js
+++ connect.js
@@ -3,29 +3,38 @@ var request = require('request-promise');
 var express = require('express');
 var nodecache = require('node-cache');
 
+// Create instance of express
 var express_app = express();
+// Create a 10 minute cache for storing user devices
 var cache = new nodecache({ stdTTL: 600, checkperiod: 120 });
-
+// Create instance of alexa-app
 var app = new alexa.app('connect');
+// Bind alexa-app to express instance
 app.express({ expressApp: express_app });
 
+// Run every time the skill is accessed
 app.pre = function (req, res, type) {
+    // Error if the application ID of the request is not for this skill
     if (req.applicationId != "amzn1.ask.skill.33d79728-0f5a-44e7-ae22-ccf0b0c0e9e0" &&
         req.getSession().details.application.applicationId != "amzn1.ask.skill.33d79728-0f5a-44e7-ae22-ccf0b0c0e9e0") {
         throw "Invalid applicationId";
     }
+    // Check that the user has an access token, if they have linked their account
     if (!req.getSession().details.user.accessToken) {
         res.say("You have not linked your Spotify account, check your Alexa app to link the account");
         res.linkAccount();
     }
 };
 
+// Function for when skill is invoked without intent
 app.launch(function (req, res) {
     res.say("I can control your Spotify Connect devices, to start, ask me to list your devices");
     res.reprompt("To start, ask me to list your devices");
     res.shouldEndSession(false);
 });
 
+// Handle default Amazon help intent
+// No slots or utterances required
 app.intent("AMAZON.HelpIntent", {
     "slots": {},
     "utterances": []
@@ -37,6 +46,8 @@ app.intent("AMAZON.HelpIntent", {
     return;
 });
 
+// Handle default Amazon stop intent
+// No slots or utterances required
 app.intent("AMAZON.StopIntent", {
     "slots": {},
     "utterances": []
@@ -44,6 +55,8 @@ app.intent("AMAZON.StopIntent", {
     return;
 });
 
+// Handle default Amazon cancel intent
+// No slots or utterances required
 app.intent("AMAZON.CancelIntent", {
     "slots": {},
     "utterances": []
@@ -51,6 +64,8 @@ app.intent("AMAZON.CancelIntent", {
     return;
 });
 
+// Handle play intent
+// No slots required
 app.intent('PlayIntent', {
     "utterances": [
         "play",
@@ -59,20 +74,26 @@ app.intent('PlayIntent', {
     ]
 },
     function (req, res) {
+        // PUT to Spotify REST API
         request.put("https://api.spotify.com/v1/me/player/play").auth(null, null, true, req.getSession().details.user.accessToken);
     }
 );
 
+// Handle pause intent
+// No slots required
 app.intent('PauseIntent', {
     "utterances": [
         "pause"
     ]
 },
     function (req, res) {
+        // PUT to Spotify REST API
         request.put("https://api.spotify.com/v1/me/player/pause").auth(null, null, true, req.getSession().details.user.accessToken);
     }
 );
 
+// Handle skip next intent
+// No slots required
 app.intent('SkipNextIntent', {
     "utterances": [
         "skip",
@@ -81,10 +102,13 @@ app.intent('SkipNextIntent', {
     ]
 },
     function (req, res) {
+        // POST to Spotify REST API
         request.post("https://api.spotify.com/v1/me/player/next").auth(null, null, true, req.getSession().details.user.accessToken);
     }
 );
 
+// Handle skip previous intent
+// No slots required
 app.intent('SkipPreviousIntent', {
     "utterances": [
         "previous",
@@ -94,10 +118,13 @@ app.intent('SkipPreviousIntent', {
     ]
 },
     function (req, res) {
+        // POST to Spotify REST API
         request.post("https://api.spotify.com/v1/me/player/previous").auth(null, null, true, req.getSession().details.user.accessToken);
     }
 );
 
+// Handle get devices intent
+// No slots required
 app.intent('GetDevicesIntent', {
     "utterances": [
         "devices",
@@ -107,32 +134,37 @@ app.intent('GetDevicesIntent', {
     ]
 },
     function (req, res) {
+        // GET from Spotify REST API
         return request.get({
             url: "https://api.spotify.com/v1/me/player/devices",
+            // Send access token as bearer auth
             auth: {
                 "bearer": req.getSession().details.user.accessToken
             },
+            // Parse results as JSON
             json: true
         })
             .then(function (body) {
                 var devices = body.devices || [];
                 var deviceNames = [];
                 for (var i = 0; i < devices.length; i++) {
-                    //Number each device
+                    // Number each device
                     deviceNames.push((i + 1) + ". " + devices[i].name);
-                    //Add the device number to JSON
+                    // Add the device number to JSON
                     devices[i].number = (i + 1);
                 }
                 req.getSession().set("devices", devices);
                 cache.set(req.getSession().details.user.userId + ":devices", devices);
+                // Check if user has devices
                 if (devices.length > 0) {
-                    //Comma separated list of device names
+                    // Comma separated list of device names
                     res.say("I found these connect devices: ");
                     res.say([deviceNames.slice(0, -1).join(', '), deviceNames.slice(-1)[0]].join(deviceNames.length < 2 ? '' : ', and ') + ". ");
                     res.say("What would you like to do with these devices?").reprompt("What would you like to do?");
                     res.shouldEndSession(false);
                 }
                 else {
+                    // No devices found
                     res.say("I couldn't find any connect devices, check your Alexa app for information on connecting a device");
                     res.card({
                         type: "Simple",
@@ -144,12 +176,15 @@ app.intent('GetDevicesIntent', {
                     });
                 }
             })
+            // Handle errors
             .catch(function (err) {
                 console.error('error:', err.message);
             });
     }
 );
 
+// Handle device play intent
+// Slot for device number
 app.intent('DevicePlayIntent', {
     "slots": {
         "DEVICENUMBER": "AMAZON.NUMBER"
@@ -159,48 +194,61 @@ app.intent('DevicePlayIntent', {
     ]
 },
     function (req, res) {
+        // Check that request contains session
         if (req.hasSession()) {
+            // Check that the slot has a value
             if (req.slot("DEVICENUMBER")) {
+                // Check if the slot is a number
                 if (!isNaN(req.slot("DEVICENUMBER"))) {
                     var deviceNumber = req.slot("DEVICENUMBER");
+                    // Check if session is new
                     if (req.getSession().isNew()) {
-                        //If new session try to use cache
+                        // If new session try to use cache
                         var devices = cache.get(req.getSession().details.user.userId + ":devices") || [];
                     }
                     else {
+                        // If existing session use session data
                         var devices = req.getSession().get("devices") || [];
                     }
                     var deviceId, deviceName;
+                    // Iterate through devices to find ID and name by number
                     for (var i = 0; i < devices.length; i++) {
                         if (devices[i].number == deviceNumber) {
                             deviceId = devices[i].id;
                             deviceName = devices[i].name;
                         }
                     }
+                    // Check that the device for the number was found
                     if (deviceId) {
+                        // PUT to Spotify REST API
                         request.put({
                             url: "https://api.spotify.com/v1/me/player",
+                            // Send access token as bearer auth
                             auth: {
                                 "bearer": req.getSession().details.user.accessToken
                             },
                             body: {
+                                // Send device ID
                                 "device_ids": [
                                     deviceId
                                 ],
+                                // Make sure that music plays
                                 "play": true
                             },
+                            // Handle sending as JSON
                             json: true
                         });
                         res.say("Playing on device " + deviceNumber + ": " + deviceName);
                     }
                     else {
+                        // If device for number not found
                         res.say("I couldn't find device " + deviceNumber + ". ");
                         res.say("Try asking me to list devices first");
                         res.shouldEndSession(false);
                     }
                 }
                 else {
-                    //Not a number        
+                    // Not a number
                     res.say("I couldn't work out which device to play on, make sure you refer to the device by number.");
                     res.say("Try asking me to play on a device number");
                     res.reprompt("What would you like to do?");
@@ -208,7 +256,7 @@ app.intent('DevicePlayIntent', {
                 }
             }
             else {
-                //No slot value
+                // No slot value
                 res.say("I couldn't work out which device number to play on.");
                 res.say("Try asking me to play on a device number");
                 res.reprompt("What would you like to do?");
@@ -218,11 +266,8 @@ app.intent('DevicePlayIntent', {
     }
 );
 
-express_app.use(express.static(__dirname));
-express_app.get('/', function (req, res) {
-    res.redirect('https://github.com/thorpelawrence/alexa-spotify-connect');
-});
-
+// Handle device transfer intent
+// Slot for device number
 app.intent('DeviceTransferIntent', {
     "slots": {
         "DEVICENUMBER": "AMAZON.NUMBER"
@@ -232,47 +277,59 @@ app.intent('DeviceTransferIntent', {
     ]
 },
     function (req, res) {
+        // Check that request contains session
         if (req.hasSession()) {
+            // Check that the slot has a value
             if (req.slot("DEVICENUMBER")) {
+                // Check if the slot is a number
                 if (!isNaN(req.slot("DEVICENUMBER"))) {
                     var deviceNumber = req.slot("DEVICENUMBER");
+                    // Check if session is new
                     if (req.getSession().isNew()) {
-                        //If new session try to use cache
+                        // If new session try to use cache
                         var devices = cache.get(req.getSession().details.user.userId + ":devices") || [];
                     }
                     else {
+                        // If existing session use session data
                         var devices = req.getSession().get("devices") || [];
                     }
                     var deviceId, deviceName;
+                    // Iterate through devices to find ID and name by number
                     for (var i = 0; i < devices.length; i++) {
                         if (devices[i].number == deviceNumber) {
                             deviceId = devices[i].id;
                             deviceName = devices[i].name;
                         }
                     }
+                    // Check that the device for the number was found
                     if (deviceId) {
+                        // PUT to Spotify REST API
                         request.put({
                             url: "https://api.spotify.com/v1/me/player",
+                            // Send access token as bearer auth
                             auth: {
                                 "bearer": req.getSession().details.user.accessToken
                             },
                             body: {
+                                // Send device ID
                                 "device_ids": [
                                     deviceId
                                 ]
                             },
+                            // Handle sending as JSON
                             json: true
                         });
                         res.say("Transferring to device " + deviceNumber + ": " + deviceName);
                     }
                     else {
+                        // If device for number not found
                         res.say("I couldn't find device " + deviceNumber + ". ");
                         res.say("Try asking me to list devices first");
                         res.shouldEndSession(false);
                     }
                 }
                 else {
-                    //Not a number
+                    // Not a number
                     res.say("I couldn't work out which device to transfer to, make sure you refer to the device by number.");
                     res.say("Try asking me to to transfer a device number");
                     res.reprompt("What would you like to do?");
@@ -280,7 +337,7 @@ app.intent('DeviceTransferIntent', {
                 }
             }
             else {
-                //No slot value
+                // No slot value
                 res.say("I couldn't work out which device number to transfer to.");
                 res.say("Try asking me to transfer to a device number");
                 res.reprompt("What would you like to do?");
@@ -290,6 +347,8 @@ app.intent('DeviceTransferIntent', {
     }
 );
 
+// Handle get track intent
+// No slots required
 app.intent('GetTrackIntent', {
     "utterances": [
         "{what is|what's} {playing|this song}",
@@ -297,11 +356,14 @@ app.intent('GetTrackIntent', {
     ]
 },
     function (req, res) {
+        // GET from Spotify REST API
         return request.get({
             url: "https://api.spotify.com/v1/me/player/currently-playing",
+            // Send access token as bearer auth
             auth: {
                 "bearer": req.getSession().details.user.accessToken
             },
+            // Parse results as JSON
             json: true
         })
             .then(function (body) {
@@ -310,26 +372,34 @@ app.intent('GetTrackIntent', {
                 }
                 else {
                     if (body.item.name) {
-                        //If not playing but last track known
+                        // If not playing but last track known
                         res.say("That was " + body.item.name + " by " + body.item.artists[0].name);
                     }
                     else {
-                        //If unknown
+                        // If unknown
                         res.say("Nothing is playing");
                     }
                 }
             })
+            // Handle errors
             .catch(function (err) {
                 console.error('error:', err.message);
             });
     }
 );
 
-//Only listen if run directly, not if required as a module
+// Set up redirect to project page
+express_app.use(express.static(__dirname));
+express_app.get('/', function (req, res) {
+    res.redirect('https://github.com/thorpelawrence/alexa-spotify-connect');
+});
+
+// Only listen if run directly, not if required as a module
 if (require.main === module) {
     var port = process.env.PORT || 8888;
     console.log("Listening on port " + port);
     express_app.listen(port);
 }
 
+// Export alexa-app instance for skill.js
 module.exports = app;
